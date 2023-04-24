04496a8470c8e4642fc544092f397fa773aa880c
Tong Tang <pichip22@hotmail.com>
2023-04-24 00:10:06 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+    const url = `http://localhost:3000/users`;
+    const data = {name: name, email: email};
+    const config = {
+        method: `POST`,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': `application/json`
+        },
+        body:JSON.stringify(data)
+    };
+    return fetch(url,config)
+        .then(response => response.json())
+        .then(data => {
+            const id = data.id;
+            document.body.innerHTML += id;
+        })
+        .catch(error => {
+            document.body.innerHTML += error.message;
+        });
+        
+}
